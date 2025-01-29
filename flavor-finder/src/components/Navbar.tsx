b3adf72ffd1ff1c7c6f96998c2ed0d7a498f2d0b
Tate <tatesutter04@gmail.com>
2025-01-28 18:45:56 -0600

    removed get started button

--- flavor-finder/src/components/Navbar.tsx
+++ flavor-finder/src/components/Navbar.tsx
@@ -1,28 +1,46 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar: React.FC<NavbarProps> = ({ isAuthenticated, onLogout }) => (
   <nav className="navbar bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 text-white shadow-lg p-4">
     <div className="navbar-start">
-      <Link to="/" className="text-2xl font-bold tracking-wide">Flavor Finder</Link>
+      <Link to="/" className="text-2xl font-bold tracking-wide">
+        Flavor Finder
+      </Link>
     </div>
     <div className="navbar-center hidden lg:flex">
       <ul className="menu menu-horizontal px-4">
-        <li><Link to="/recipes?search=saved" className="hover:underline">Saved Recipes</Link></li>
+        <li>
+          <Link to="/recipes?search=saved" className="hover:underline">
+            Saved Recipes
+          </Link>
+        </li>
         {/* <li><Link to="/profile" className="hover:underline">Profile</Link></li> */}
         {isAuthenticated ? (
-          <li><button onClick={onLogout} className="hover:underline">Logout</button></li>
+          <li>
+            <button onClick={onLogout} className="hover:underline">
+              Logout
+            </button>
+          </li>
         ) : (
           <>
-            <li><Link to="/login" className="hover:underline">Login</Link></li>
-            <li><Link to="/signup" className="hover:underline">Signup</Link></li>
+            <li>
+              <Link to="/login" className="hover:underline">
+                Login
+              </Link>
+            </li>
+            <li>
+              <Link to="/signup" className="hover:underline">
+                Signup
+              </Link>
+            </li>
           </>
         )}
       </ul>
     </div>
-    <div className="navbar-end">
+    {/* <div className="navbar-end">
       <button className="btn bg-white text-blue-500 hover:bg-gray-200">Get Started</button>
-    </div>
+    </div> */}
   </nav>
 );
 
